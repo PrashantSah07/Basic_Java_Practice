35d9ad880f5e166b6d7c33b7d1249e3c31b66b41
prashant <prashantkumar855114@gmail.com>
2024-01-25 16:34:09 +0530

    updated app.java

--- App.java
+++ App.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 class App {
-  public static void main (String args[]) {
+
+  public static void main(String args[]) {
     System.out.println("hello world");
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the first number:");
+    int a = sc.nextInt();
+
+    System.out.println("Enter the second number:");
+    int b = sc.nextInt();
+
+    System.out.println("Before Swapping: " + a + " " + b);
+
+    int temp = a;
+    a = b;
+    b = temp;
+
+    System.out.println("After Swapping: " + a + " " + b);
   }
 }
