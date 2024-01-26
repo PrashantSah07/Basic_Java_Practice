a8bb05e8a3502d1d1aab1b42d9614f99efaec3b5
prashant <prashantkumar855114@gmail.com>
2024-01-26 16:02:55 +0530

    Learn Inheritance.

--- App.java
+++ App.java
@@ -1,22 +1,40 @@
-import java.util.Scanner;
+class Base {
 
+  private int x;
+
+  void setX(int a) {
+    x = a;
+  }
+
+  int getX() {
+    return x;
+  }
+
+  void printme() {
+    System.out.println("I am a constructor of class Base " + x);
+  }
+}
+ 
+class Derived extends Base{  
+  private int y; 
+
+  void setY(int b){ 
+    y=b;
+  } 
+
+  int getY(){ 
+    return y;
+  }
+
+}
 class App {
 
-  public static void main(String args[]) {
-    System.out.println("hello world");
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter the first number:");
-    int a = sc.nextInt();
+  public static void main(String[] args) {
+    Base b = new Base();
+    b.setX(5);
+    b.printme();
 
-    System.out.println("Enter the second number:");
-    int b = sc.nextInt();
-
-    System.out.println("Before Swapping: " + a + " " + b);
-
-    int temp = a;
-    a = b;
-    b = temp;
-
-    System.out.println("After Swapping: " + a + " " + b);
+    System.out.println(b.getX()); 
+    b.setY(10);
   }
 }
