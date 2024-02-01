ad05994b882ca617a4acf95b5dbaa770d2c879fb
prashant <prashantkumar855114@gmail.com>
2024-02-01 18:37:08 +0530

    Learned Method_Dispatch

---
+++ Method_Dispatch.java
@@ -0,0 +1,42 @@
+class Phone {
+
+  void on() {
+    System.out.println("Turning on the Phone...");
+  }
+
+  void greet() {
+    System.out.println("Good Morning");
+  }
+}
+
+class SmartPhone extends Phone {
+
+  void music() {
+    System.out.println("Playing Music...");
+  }
+
+  void on() {
+    System.out.println("Turning on the Smartphone...");
+  }
+}
+
+class Method_Dispatch {
+
+  public static void main(String[] args) {
+    // Phone pn = new Phone();
+    // SmartPhone spn = new SmartPhone();
+    // pn.on();
+    // pn.greet();
+
+    // spn.music();
+    // spn.on();
+
+      Phone pn = new SmartPhone(); //---> This is valid
+      //  SmartPhone smp=new Phone();  //---> This is not valid
+
+      pn.greet();
+      pn.on();
+      // pn.swagat();   //---> Not Allowed
+
+  }
+}
