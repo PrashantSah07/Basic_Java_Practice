806bb4362bcf8d27e60711dcfce2b9b169d49cb9
prashant <prashantkumar855114@gmail.com>
2024-02-01 20:33:22 +0530

    Updated App.java

--- App.java
+++ App.java
@@ -1,24 +1,5 @@
-class game {
-
-  public int n;
-
-   game(int n) {
-    this.n= n;
-  }  
-  void setN(int a){ 
-   n=a;
-  }
-  void getN(){ 
-    System.out.println("The value of n is "+n);
-  }
-}
-
-class App {
-
-  public static void main(String[] args) { 
-    game g=new game(7); 
-    g.getN();
-    g.n=5;
-    g.getN();
+class App{ 
+  public static void main(String[] args) {
+    System.out.println();
   }
 }
