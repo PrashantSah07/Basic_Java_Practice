0f0666f7d8114752720ccc44f82e518f50bd9b32
prashant <prashantkumar855114@gmail.com>
2024-02-05 19:36:38 +0530

    Learned Interference.java

---
+++ Interfaces.java
@@ -0,0 +1,82 @@
+// interface Bicycle {                            // You can'nt define a class in interfaces & also can't create an onject.
+//   // void gear(){                             // When you create an interface class then you can'nt make it's own method like (abstracts class).
+//   //     System.out.println("Changing gear");
+//   // }
+
+//   void speedUP(int increment);
+
+//   void Break(int decremant);
+// }
+
+// class AvonCycle implements Bicycle {        // abstracts class using extends keyword during inherite & interfaces using implements keyword.
+
+//   void blowHorn() {
+//     System.out.println("Pee-Pee Poo-Poo");
+//   }
+
+//   public void speedUP(int a) {                                        // When some child class inherite parents class method then must be declared public in interface.
+//     System.out.println("Applying SpeedUP with " + a + " rpm");
+//   }
+
+//   public void Break(int b) {
+//     System.out.println("Applying Break with " + b + " rpm");
+//   }
+// }
+
+// public class Interfaces {
+
+//   public static void main(String[] args) {
+//     AvonCycle av = new AvonCycle();
+//     av.blowHorn();
+//     av.speedUP(23);
+//     av.Break(12);
+//   }
+// }
+
+
+
+interface Home {
+  void sleeping();
+  void eating();
+}
+
+interface Home2 {
+  void dancing();
+  void watching();
+}
+
+class Ghar implements Home, Home2 {
+
+  void benefits() {
+    System.out.println("Using interfaces we can implements one or more interface classes in one child class.");
+  }
+
+  public void sleeping() {
+    System.out.println("I am sleeping");
+  }
+
+  public void eating() {
+    System.out.println("I am eating");
+  }
+
+  public void dancing() {
+    System.out.println("I am dancing");
+  }
+
+  public void watching() {
+    System.out.println("I am watching movie");
+  }
+}
+
+public class Interfaces {
+
+  public static void main(String[] args) {
+    Ghar gh = new Ghar();
+    gh.benefits();
+
+    gh.sleeping();
+    gh.eating();
+    gh.dancing();
+    gh.watching();
+  }
+}
