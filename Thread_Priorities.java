f1cab350947077ad8322a166b711227aeea60ed5
prashant <prashantkumar855114@gmail.com>
2024-03-24 10:25:25 +0530

    Learned Thread_Priorities

--- Thread_Priorities.java
+++ Thread_Priorities.java
@@ -20,17 +20,18 @@ public class Thread_Priorities {
     Mythrr t3 = new Mythrr("Ayush");
     Mythrr t4 = new Mythrr("Piyush");
     Mythrr t5 = new Mythrr("Ritik");
-
+    
+    t1.setPriority(Thread.MAX_PRIORITY);
+    t2.setPriority(Thread.MIN_PRIORITY);
+    t3.setPriority(Thread.MIN_PRIORITY);
+    t4.setPriority(Thread.MIN_PRIORITY);
+    t5.setPriority(Thread.MIN_PRIORITY); 
+    
     t1.start();
     t2.start();
     t3.start();
     t4.start();
     t5.start();
 
-    t1.setPriority(Thread.MAX_PRIORITY);
-    t2.setPriority(Thread.MIN_PRIORITY);
-    t3.setPriority(Thread.MIN_PRIORITY);
-    t4.setPriority(Thread.MIN_PRIORITY);
-    t5.setPriority(Thread.MIN_PRIORITY);
   }
 }
